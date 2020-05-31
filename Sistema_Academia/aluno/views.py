d0ffbd4ce4271ca088518b78df6ffa4bbbc0cacc
sinval <sinvalvieirajunior@gmail.com>
2020-05-31 18:27:00 -0300

    Adiciona exibicao do treino do aluno no Home

--- Sistema_Academia/aluno/views.py
+++ Sistema_Academia/aluno/views.py
@@ -5,4 +5,14 @@ from .models import Aluno
 def home(request):
     user_id = request.session['user_logged_id']
     user = Aluno.objects.filter(id=user_id).first()
-    return render(request,'aluno/home.html',{'user_logged':user})
+    
+    try:
+        treino = user.treino.get()
+    except:
+        treino = None
+
+    context = {
+        'user_logged': user,
+        'treino': treino
+    }
+    return render(request,'aluno/home.html',context)
