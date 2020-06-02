7e6e0258afacf8b6fb96c81631d2fb5f74a354a6
sinval <sinvalvieirajunior@gmail.com>
2020-06-01 21:32:33 -0300

    Adiciona exibição do quadro de horários do aluno no Home

--- Sistema_Academia/aluno/views.py
+++ Sistema_Academia/aluno/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from academia.models import Academia_Users 
-from .models import Aluno
+from .models import Aluno,Aula
 
 def home(request):
     user_id = request.session['user_logged_id']
@@ -11,8 +11,42 @@ def home(request):
     except:
         treino = None
 
+
+    all_aulas = Aula.objects.filter()
+    aulas_aluno = []
+
+    for aula in all_aulas:
+        if(user in aula.alunos.all()):
+            aulas_aluno.append(aula)
+    
+    horarios = []
+    for aula in aulas_aluno:
+        if aula.horario not in horarios:
+            horarios.append(aula.horario)
+
+    horarios.sort()
+    tabela = []
+    dias_uteis = ["Seg", "Ter", "Qua", "Qui", "Sex"]
+    has_aula = False
+    for horario in horarios:
+        aulas_do_dia = []
+        for dia in dias_uteis:
+            has_aula = False
+            for aula in aulas_aluno:
+                if dia in aula.dia and horario in aula.horario:
+                    aulas_do_dia.append(aula)
+                    has_aula = True
+            if not has_aula:
+                aulas_do_dia.append([])
+
+    
+        tabela.append(aulas_do_dia)
+            
     context = {
         'user_logged': user,
-        'treino': treino
+        'treino': treino,
+        'has_aula': len(aulas_aluno),
+        'horarios': horarios,
+        'tabela': tabela,
     }
     return render(request,'aluno/home.html',context)
